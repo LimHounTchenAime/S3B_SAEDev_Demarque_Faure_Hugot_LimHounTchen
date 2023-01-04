8e962000ce0dc4ba295b432f0839a93c20e3447b
FAURE Solene <solene.faure1@etu.univ-lorraine.fr>
2023-01-04 17:11:23 +0100

    Ajout d'un programme principal servant aux tests d'affichage

--- src/test/Modele/Principale.java
+++ src/test/Modele/Principale.java
@@ -1,2 +1,25 @@
-package test.Modele;public class Principale {
+package test.Modele;
+
+import java.Modele.Abstract;
+import java.Modele.Concrete;
+import java.Modele.Interface;
+
+
+public class Principale {
+
+    public static void main(String[] args) {
+
+        Concrete conc = new Concrete();
+        Abstract abs = new Abstract();
+        Interface inter = new Interface();
+
+        System.out.println("Affichage classe Concrete \n");
+        System.out.println(conc.toString());
+
+        System.out.println("Affichage classe Abstract \n");
+        System.out.println(abs.toString());
+
+        System.out.println("Affichage classe Interface \n");
+        System.out.println(inter.toString());
+    }
 }
