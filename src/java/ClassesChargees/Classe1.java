0f98e59fe5766b65e5684982606ba7448c6eb16c
Aimé <aimelimhountchen@gmail.com>
2023-01-11 09:47:17 +0100

    Ajout Classe1

---
+++ src/java/ClassesChargees/Classe1.java
@@ -0,0 +1,15 @@
+package ClassesChargees;
+
+public class Classe1 {
+    int a;
+    String b;
+
+    Classe1(int a, String b){
+        this.a=a;
+        this.b=b;
+    }
+
+    public void show(){
+        System.out.println(a+b);
+    }
+}
